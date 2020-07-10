cb1b39d13b8019645cd28c8b903eb037c8d070c4
LifeEternal <48825766+LifeEternal@users.noreply.github.com>
2020-07-10 16:37:30 +0100

    Changes to .h and .m

--- src/ios/AppDelegate+FirebaseDynamicLinksPlugin.m
+++ src/ios/AppDelegate+FirebaseDynamicLinksPlugin.m
@@ -5,33 +5,15 @@
 
 @implementation AppDelegate (FirebaseDynamicLinksPlugin)
 
-// Borrowed from http://nshipster.com/method-swizzling/
-+ (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        Class class = [self class];
-        
-        SEL originalSelector = @selector(application:continueUserActivity:restorationHandler:);
-        SEL swizzledSelector = @selector(identity_application:continueUserActivity:restorationHandler:);
-        
-        Method originalMethod = class_getInstanceMethod(class, originalSelector);
-        Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
-        
-        BOOL didAddMethod =
-        class_addMethod(class,
-                        originalSelector,
-                        method_getImplementation(swizzledMethod),
-                        method_getTypeEncoding(swizzledMethod));
-        
-        if (didAddMethod) {
-            class_replaceMethod(class,
-                                swizzledSelector,
-                                method_getImplementation(originalMethod),
-                                method_getTypeEncoding(originalMethod));
-        } else {
-            method_exchangeImplementations(originalMethod, swizzledMethod);
-        }
-    });
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<NSString *, id> *)options {
+    FirebaseDynamicLinksPlugin* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
+    FIRDynamicLink *dynamicLink = [[FIRDynamicLinks dynamicLinks] dynamicLinkFromCustomSchemeURL:url];
+
+    if (dynamicLink) {
+        [dl postDynamicLink:dynamicLink];
+        return YES;
+    }
+    return NO;
 }
 
 - (BOOL)application:(UIApplication *)app continueUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
@@ -51,4 +33,3 @@
             }
         }];
 }
-@end
