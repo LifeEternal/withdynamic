e58593f3f0c72ffa1914a36d016c8ac1fdca6012
LifeEternal <48825766+LifeEternal@users.noreply.github.com>
2020-07-10 12:48:08 +0100

    Update AppDelegate+FirebaseDynamicLinksPlugin.m

--- src/ios/AppDelegate+FirebaseDynamicLinksPlugin.m
+++ src/ios/AppDelegate+FirebaseDynamicLinksPlugin.m
@@ -35,14 +35,14 @@
 }
 
 // [START continueuseractivity]
+
 - (BOOL)identity_application:(UIApplication *)application
         continueUserActivity:(NSUserActivity *)userActivity
           restorationHandler:(void (^)(NSArray *))restorationHandler {
     __block FirebaseDynamicLinksPlugin* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
 
-    BOOL handled = [[FIRDynamicLinks dynamicLinks]
-        handleUniversalLink:userActivity.webpageURL
-        completion:^(FIRDynamicLink * _Nullable dynamicLink, NSError * _Nullable error) {
+    BOOL handled = [[FIRDynamicLinks dynamicLinks] handleUniversalLink:userActivity.webpageURL 
+	completion:^(FIRDynamicLink * _Nullable dynamicLink, NSError * _Nullable error) {
             // Try this method as some dynamic links are not recognize by handleUniversalLink
             // ISSUE: https://github.com/firebase/firebase-ios-sdk/issues/743
             if(dl == nil) {             
